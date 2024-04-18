a57b0651a5b2a6931383c64f9239e1b533b505da
Bob's & Co <33040669+shoot1101@users.noreply.github.com>
2024-04-18 19:19:40 +0200

    Update server.lua

--- Doorbell/server/server.lua
+++ Doorbell/server/server.lua
@@ -2,16 +2,16 @@ ESX = exports["es_extended"]:getSharedObject()
 
 RegisterNetEvent('es_doorbell:NotifyJob')
 AddEventHandler('es_doorbell:NotifyJob', function(job)
-
     local xPlayers = ESX.GetPlayers()
 
     for i=1, #xPlayers, 1 do
         local xPlayer = ESX.GetPlayerFromId(xPlayers[i])
-        TriggerClientEvent('ox_lib:notify', xPlayer.source, {title = Config.Bell, description = Config.YouRang, duration = 5000, type = 'success'})  
-        if xPlayer.getJob().name == job then
-            TriggerClientEvent('ox_lib:notify', xPlayer.source, {title = Config.Bell, description = Config.SomeoneRang, duration = 5000, type = 'inform'})
-            TriggerClientEvent('InteractSound_CL:PlayOnOne', xPlayer.source, Config.SoundName, 0.1)
+        if xPlayer then
+            TriggerClientEvent('ox_lib:notify', xPlayer.source, {title = Config.Bell, description = Config.YouRang, duration = 5000, type = 'success'})  
+            if xPlayer.getJob().name == job then
+                TriggerClientEvent('ox_lib:notify', xPlayer.source, {title = Config.Bell, description = Config.SomeoneRang, duration = 5000, type = 'inform'})
+                TriggerClientEvent('InteractSound_CL:PlayOnOne', xPlayer.source, Config.SoundName, 0.1)
+            end
         end
     end
-
 end)
