099c2fa4e6a4a7e65bea94a4092532a9a889e881
Bob's & Co <33040669+shoot1101@users.noreply.github.com>
2024-06-20 11:28:51 +0200

    Update config.lua

--- Doorbell/config.lua
+++ Doorbell/config.lua
@@ -1,62 +1,46 @@
 Config = {}
 
+Config.debug = true
 Config.Bell = "Doorbell"
 Config.YouRang = "You rang the bell"
 Config.SomeoneRang = "Someone rang the bell"
 Config.Ring = "[E] - Doorbell"
+Config.CameraTextKey = "[E] - Open Camera"
+
 Config.Wait1 = "Wait "
 Config.Wait2 = " seconds"
 Config.UseInteractSound = true --REQUIRES: https://github.com/plunkettscott/interact-sound/releases 
 Config.SoundName = "doorbell"
 Config.DrawDistance    = 10.0
 
--- // SETTINGS //
-
 -- Put here the time the player needs to wait before he can ring again (in seconds)
-Config.WaitingTime = 30
+Config.WaitingTime = 15
 
--- //  SETTINGS BELLS //
+-- SETTINGS BELLS
 
 Config.Bells = {
-
     {
-        coords = vec3(-283.87, 281.44, 88.89),  -- Enter bell position
+        coords = vec3(431.53, -985.4, 30.73),  -- Enter bell position 
+        bellcoords = vec3(431.63, -985.3, 30.73),  -- Doorbell prop position
+        heading = 270.0,  --Props angle
         job = 'police'  -- Enter job name here
     },
-
     {
-        coords = vec3(300.2, -581.32, 42.36),  
-        job = 'ambulance'
+        coords = vec3(431.53, -985.4, 30.73),  -- Enter bell position 
+        bellcoords = vec3(431.63, -985.2, 30.73),  -- Doorbell prop position
+        heading = 270.0,  --Props angle
+        job = 'ambulance'  -- Enter job name here
     },
-	
-	{
-        coords = vec3(58.18, -1581.44, 28.6),   
-        job = 'mechanic'
-    },
-	
-	{
-        coords = vec3(-543.82, -203.32, 37.32),     
-        job = 'gouvernement'
-    },
-	
-	{
-        coords = vec3(170.78, -1044.42, 28.32),  
-        job = 'vigneron'
-    },
-	
-	{
-        coords = vec3(130.19, -1298.31, 28.24),
-        job = 'nightclub'
-    },
-
-    {
-        coords = vec3(908.88, -158.87, 73.25),
-        job = 'taxi'
-    },
-
-    {
-        coords = vec3(-710.42, -916.94, 18.51),
-        job = 'shop'
-    }
+    -- Add other bells with contact details and corresponding jobs here
 }
 
+-- SETTINGS CAMERA POSITIONS
+
+Config.CameraPositions = {
+    police = { 
+        position = vector3(453.7, -983.95, 30.73), -- Point to view camera
+        rotation = vector3(0.0, 0.0, 90.0), -- Camera direction angle (x, y, z)
+        viewPosition = vector3(431.53, -985.4, 30.73) -- Camera view position
+    },
+    -- Add camera positions for other jobs here
+}
